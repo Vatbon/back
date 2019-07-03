7d452d7b5218027c665f9ae1a5e1cef166c43157
vatbon <stavaton@gmail.com>
2019-07-03 11:28:07 +0700

    commit adding '.' dirs

--- src/main/java/ftc/shift/sample/api/AuthContoller.java
+++
@@ -1,9 +0,0 @@
-package ftc.shift.sample.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AuthContoller {
-
-    Use
-}
---
+++ src/main/java/ftc/shift/sample/api/AuthController.java
@@ -0,0 +1,16 @@
+package ftc.shift.sample.api;
+
+import ftc.shift.sample.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+//GET /api/auth and i get {"name":"string";} i must give back full body of User
+    @GetMapping
+    public ResponseEntity<User> authenticate(
+            @Request
+    )
+}
